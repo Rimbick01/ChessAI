a9bc38bf9d8932f01ddf6337d8e347d593399ba0
Rimbick <tushard.pe.ug@jadavpuruniversity.in>
2025-06-12 21:03:41 +0530

    add neural net

--- generate_traning_set.py
+++ generate_traning_set.py
@@ -4,16 +4,28 @@ import chess
 import chess.pgn
 from state import State
 
-for fn in os.listdir("data"):
-    pgn=open(os.path.join("data", fn))
-    while 1:
-        try:
-            game=chess.pgn.read_game(pgn)
-        except exception:
-            break
-        value={'1/2-1/2':0,'0-1':-1,'1-0':1}[game.headers['Result']]
-        board=game.board()
-        for i,move in enumerate(game.mainline_moves()):
-            board.push(move)
-            print(value,State(board).serialized()[:,:,0])
-    break
+def get_dataset(num_samples=None):
+    x,y=[],[]
+    ng=0
+    for fn in os.listdir("data"):
+        pgn=open(os.path.join("data", fn))
+        while 1:
+            try:
+                game=chess.pgn.read_game(pgn)
+            except exception:
+                break
+            value={'1/2-1/2':0,'0-1':-1,'1-0':1}[game.headers['Result']]
+            board=game.board()
+            for i,move in enumerate(game.mainline_moves()):
+                board.push(move)
+                ser=State(board).serialized()[:,:,0]
+                x.append(ser)
+                y.append(value)
+            print("parsing game %d got %d examples" %(ng,len(x)))
+            if num_samples is not None and len(x)>num_samples:
+                return x,y
+            ng+=1
+    return x,y
+if __name__=="__main__":
+    X,Y=get_dataset(1000)
+
